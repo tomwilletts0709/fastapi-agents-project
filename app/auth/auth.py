f1440b065420a86017ce49f089a40a26b87bd421
Ashleigh Hewitt <tom_94_oasis@hotmail.com>
2026-03-12 23:37:25 +0000

    starting auth

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -1,5 +1,18 @@
+from typing import Annotated
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
 from jose import JWTError, jwt
+from passlib.context import CryptContext
 
 
+router = APIRouter(
+    prefix="/auth",
+    tags=["auth"],
+)
 
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
