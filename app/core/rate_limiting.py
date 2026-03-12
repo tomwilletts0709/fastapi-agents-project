2588c34d33e8cbebf0f8376f0f91f9c7aa90d9c5
Ashleigh Hewitt <tom_94_oasis@hotmail.com>
2026-03-12 23:46:07 +0000

    adding rate limiting

--- app/core/rate_limiting.py
+++ app/core/rate_limiting.py
@@ -2,13 +2,38 @@
 from fastapi import Request
 from fastapi.responses import JSONResponse
 from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.responses import Response
+import redis.asyncio as redis
 
-"""adding boilerplate code for rate limiting"""
 
 class RateLimitingMiddleware(BaseHTTPMiddleware):
-    def __init__(self, app: FastAPI):
+    def __init__(self, redis_url: str, max_requests: int = 5, time_window: int = 60):
         super().__init__(app)
-        self.redis_client = Redis(host="localhost", port=6379, db=0)
+        self.max_requests = max_requests
+        self.time_window = time_window
+        self.redis_client = redis.from_url(redis_url, decode_responses=True)
 
-    async def dispatch(self, request: Request, call_next: Callable):
-        return await call_next(request)
+    async def dispatch(self, request: Request, call_next: Callable) -> None:
+        client_ip = request.client.host
+        key = f"rate_limit:{client_ip}"
+
+        try:
+            current_count = await self,redis_client.incr(key)
+
+            if current_count == 1: 
+                await self.redis_client.expire(key, self.time_window)
+            
+            if current_count > self.max_requests:
+                ttl = await self.redis_client.ttl(key)
+                return JSONResponse(
+                    status_code=429,
+                    content={"detail": "Too Many Requests", "retry_after": ttl},
+                )
+
+            return await call_next(request)
+        except Exception as e:
+            return JSONResponse(
+                status_code=500, content={"detail": "Internal Server Error"})
+
+        response = await call_next(request)
+        return response
