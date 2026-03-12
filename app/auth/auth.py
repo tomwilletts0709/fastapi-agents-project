bb247278147eacbc6f862b418989ac0a35ecc43e
Ashleigh Hewitt <tom_94_oasis@hotmail.com>
2026-03-12 23:41:41 +0000

    adding to auth

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -3,6 +3,9 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from jose import JWTError, jwt
 from passlib.context import CryptContext
+from fastapi import APIRouter, Depends, HTTPException, status
+from app.models.db import get_db
+from datetime import timedelta
 
 
 router = APIRouter(
@@ -16,3 +19,34 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 class CreateUserRequest(BaseModel):
     username: str
     email: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+    user_id: int
+    expires_delta: timedelta
+
+
+
+
+@router.post("/token", response_model=Token)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user: 
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+        )
+
+def authenticate_user(username: str, password: str):
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.password):
+        return False
+    return user
+
+def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+    encode = {"sub": username, "id": user_id}
+    expires = datetime.utcnow() + expires_delta
+    encode.update({"exp": expires})
+    return jwt.encode(encode, settings.secret_key, algorithm="HS256")
