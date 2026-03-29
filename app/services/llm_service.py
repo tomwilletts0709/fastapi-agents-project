8fb967bf17b8a301de84999aa79a6564a4807f3e
Ashleigh Hewitt <tom_94_oasis@hotmail.com>
2026-03-29 18:52:20 +0100

    project methods

--- app/services/llm_service.py
+++ app/services/llm_service.py
@@ -5,7 +5,7 @@ from fastapi import HTTPException
 from pydantic_ai import ModelRequest, ModelResponse, TextPart, UserPromptPart
 
 from app.ai.agents import chat_agent
-from app.models.models import Conversation, Message
+from app.models.models import Conversation, Message, Project
 
 
 class LLMService:
@@ -85,4 +85,48 @@ class LLMService:
         session.add_all([user_message, agent_message])
         await session.commit()
 
-    
+    async def create_conversation(
+        self, 
+        session: AsyncSession,
+        user_id: int, 
+        agent_id: int,
+    ) -> int: 
+        """ createt a new conversation with agent """
+        create_conversation = Conversation(user_id=user_id, agent_id=agent_id)
+        session.add(create_conversation)
+        await session.commit()
+        await session.refresh(create_conversation)
+        return create_conversation.id
+
+    async def get_conversation(
+        self, 
+        sesssion: AsyncSession,
+        user_id: int,
+        conversation_id: int, 
+    ) -> Conversation | None:
+        """ grab the conversation for a user """
+        session.execute(select(Conversation).where(Conversation.user_id == user_id, Conversation.id == conversation_id))
+        return result.scalar_one_or_none()
+
+    async def delete_conversation(
+        self, 
+        session: AsyncSession, 
+        user_id: int,
+        conversation_id: int,
+    ) -> None: 
+        """ delete the conversation for a user """
+        session.execute(select(Conversation).where(Conversation.user_id == user_id, Conversation.id == conversation_id))
+        return result.scalar_one_or_none()
+        await session.commit()
+        await session.refresh(delete_conversation)
+        return delete_conversation.id
+
+    async def create_project(
+        self, 
+        session: AsyncSession,
+        project_name: str,
+        project_description: str,
+        user_id: int,
+    ) -> int: 
+        """ create a new project for a user """
+        create_project = Project 
